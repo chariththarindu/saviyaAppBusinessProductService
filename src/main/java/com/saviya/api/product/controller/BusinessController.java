40f249009eee1116c3791619c1bed67127d52faf
charith <chariththarindu@gmail.com>
2020-01-28 23:07:08 +0530

    hystrix fall back method implementation

--- src/main/java/com/saviya/api/product/controller/BusinessController.java
+++ src/main/java/com/saviya/api/product/controller/BusinessController.java
@@ -3,6 +3,8 @@ package com.saviya.api.product.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,11 @@ import com.saviya.api.product.dto.RequestDto;
 import com.saviya.api.product.dto.ResponseDto;
 import com.saviya.api.product.service.BusinessService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
-@RequestMapping("v1/hela/business")
+@RequestMapping("v1/saviya/business")
+@Slf4j
 public class BusinessController {
 
 	private final BusinessService businessService;
@@ -24,7 +29,7 @@ public class BusinessController {
 	}
 
 	@PostMapping()
-	public ResponseEntity<?> addBusiness(@RequestBody RequestDto requestDto) {
+	public ResponseEntity<?> addBusinessProduct(@RequestBody RequestDto requestDto) {
 
 		ResponseDto res = businessService.addBusiness(requestDto);
 
@@ -32,4 +37,19 @@ public class BusinessController {
 
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getBusinessProduct(@PathVariable String id) {
+
+		ResponseDto res = ResponseDto.builder().code("101").message("response from :-" + id).build();
+
+		return new ResponseEntity<ResponseDto>(res, HttpStatus.OK);
+	}
+	
+	@GetMapping()
+	public ResponseEntity<?> getBusinessProduct() {
+
+		log.info("get business product call...");
+		return new ResponseEntity<String>("status check.....:- working..", HttpStatus.OK);
+		
+	}
 }
